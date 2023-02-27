61aca6cf9fa4c02d87b00339a27f87d91f35d2bd
Egsagon <83862309+Egsagon@users.noreply.github.com>
2023-02-27 14:15:56 +0000

    refactor

--- main.py
+++ main.py
@@ -1,72 +1,193 @@
-'''
-https://discord.com/api/oauth2/authorize?client_id=1064590959016423424&permissions=3072&scope=bot
-'''
-
+import os
 import json
+import plot
 import string
 import discord
-from collections import OrderedDict
+import datetime
+import platform
+import threading
+from numerize import numerize
+
+logs = open('bot.log', 'a', encoding = 'utf-8')
 
 # Settings
 triggers = {'GROS': ['né', 'ner', 'nez', 'nai', 'nait', 'née', 'naie', 'nées', 'nés', 'net',
                      'néz', 'ney', 'nei', 'nes', 'nais'],
-            'GARS': ['ni','ny', 'nit']}
+            'GARS': ['ni','ny', 'nit', 'nis', 'nig', 'nie']}
 
 board: dict = json.load(open('board.json'))
 
 # Initialize client
 client = discord.Client(intents = discord.Intents.all())
 
+def debug(text: str, inst: str = 'serv') -> None:
+    
+    ortext = text
+    global logs
+    
+    # Print message
+    text = text.split()
+    for i, word in enumerate(text):
+        if word[0] == '$':
+            text[i] = f'\033[92m{word}\033[0m'.replace('$', '')
+    text = ' '.join(text)
+    # print(f'[\033[35m{inst.upper(): ^6}\033[0m] {text}')
+    print(f'[\033[35m{inst.upper()}\033[0m] {text}')
+    
+    # Save to logs
+    now = datetime.datetime.now()
+    raw = now.strftime('%H:%M:%S %d/%m/%Y')
+    logs.write(f'[{raw}] [{inst}] -> {ortext}\n')
+    logs.flush()
+
 @client.event
-async def on_ready(): print('Bot ready')
+async def on_ready():
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
+    debug('Bot started')
+
+nrz = lambda int_: numerize.numerize(int_).lower()
 
 @client.event
 async def on_message(msg: discord.Message) -> None:
     
-    # Show scoreboard
-    if msg.content.startswith('$stats'):
-        
-        pad = 10
-        ebd = discord.Embed(title = f'Stats{pad * " "}GROS, GARS')
-        
-        # Sort items
-        items = list(board.items())
-        items.sort(key = lambda l: l[1]['GROS'] + l[1]['GARS'], reverse = 1)
-        
-        for author, counts in items:
+    try:
+        # Show scoreboard
+        if msg.content.startswith('$stats'):
+                        
+            res = '```json\n' # will color digits
+            length = 20
             
-            # TODO - Crappy, refactor
-            a, b = counts.values()
-            x = list(f"{f'{a}, {b}': >{pad + 18}}")
+            # Sort items
+            items = [(a, list(c.values())) for a, c in board.items()]
+            items = [(a, (*c, sum(c))) for a, c in items]
             
-            for i, char in enumerate(author): x[i] = char
-            x = ''.join(x)
+            # items.sort(key = lambda l: sum(l[1]), reverse = 1)
+            items.sort(key = lambda l: l[1][-1], reverse = 1)
             
-            ebd.add_field(name = f'```{x}```', value = '', inline = False)
+            # Get totals
+            total_a = sum([c[0] for _, c in items])
+            total_b = sum([c[1] for _, c in items])
+            
+            items = [(a, list(map(nrz, c))) for a, c in items]
+            
+            values = [str(c[0]) for _, c in items]
+            values_b = [str(c[1]) for _, c in items]
+            values.sort(key = len, reverse = 1)
+            values_b.sort(key = len, reverse = 1)
+            max_len_a = len(values[0])
+            max_len_b = len(values_b[0])
+            
+            # Ensure there is place for titles
+            max_len_a = max(4, max_len_a)
+            max_len_b = max(4, max_len_b)
+            
+            # Set title
+            items.insert(0, (' LEADERBOARD ', (' né ', ' ni ', ' sum ')))
+            
+            for i, (author, (a, b, s)) in enumerate(items):
+                
+                # Scoreboard length
+                if i > 10: break
+                
+                # Remove weird author chars
+                if i:
+                    for char in author:
+                        if char not in string.printable or not char.strip():
+                            author = author.replace(char, '')
+                
+                if i:		res += f'{i: >2}. {author: <{length}} ┃ {a: <{max_len_a}} ┃ {b: <{max_len_b}} ┃ {s}\n'
+                else:		res += f' ┏━━{author:━^{length}}━┳━{a:━<{max_len_a}}━┳━{b:━<{max_len_b}}━┳━{s}━┓\n'
+                if i == 10:	res += f' ┗━━{"":━<{length}}━┻━{"":━<{max_len_a}}━┻━{"":━<{max_len_b}}━┻━━━━━━━┛\n'
+                
+            res += f'\nTOTAL: {nrz(total_a)} (né) + {nrz(total_b)} (ni) = {nrz(total_a + total_b)}'
+            await msg.reply(f'{res}```')
+            
+            # Generate graph
+            graph = plot.build('', dpi = 50)
+            
+            with open(graph, 'rb') as f:
+                await msg.reply(file = discord.File(f))
+            
+            return debug(f'Sending scoreboard for ${msg.author.name} on ${msg.guild.name}', 'NE_STAT')
         
-        return await msg.channel.send(embed = ebd)
+        # Show json scoreboard
+        if msg.content.startswith('$jstats'):
+            raw = json.dumps(board, indent = 2)
+            
+            try:
+                await msg.author.send(f'```json\n{raw}```')
+                await msg.reply('Sent.')
+                
+            except:
+                await msg.reply('Ouvre tes mp grocon')
+            
+            debug(f'Send json stats to ${msg.author}', 'NE_STAT')
+        
+        # Show user stats
+        if msg.content.startswith('$mstats'):
+            stats = board[msg.author.name]
+            
+            wds = msg.content.split()
+			
+            if len(wds) == 2:
+                kwargs = eval(wds)
+            else:
+                kwargs = {}
+            
+            graph = plot.build(msg.author.name, **kwargs)
+            
+            await msg.reply(f'Stats de {msg.author.name}:\n> GROS ↣ {stats["GROS"]}\n> GARS ↣ {stats["GARS"]}')
+            
+            with open(graph, 'rb') as f:
+                await msg.channel.send(file = discord.File(f))
+            
+            debug(f'Sent graph to ${msg.author.name}', 'NE_grph')
+        
+        # Check for triggers
+        t = msg.content.lower()
+        for char in string.punctuation:
+            t = t.replace(char, '')
+        t = t.strip()
+        
+        #for emoji in msg.guild.emojis:
+        #    print(emoji.name, emoji.id)
+        
+        for response, trgs in triggers.items():
+            for trigger in trgs:
+                
+                if t.endswith(trigger):
+                    
+                    if isinstance(msg.channel, discord.channel.DMChannel):
+                        debug(f'${msg.author.name} attempted to dm')
+                        return await msg.reply('PATATE')
+                    
+                    await msg.reply('**' + response + '**')
+                    
+                    # Save to scoreboard
+                    author = msg.author.name
+                    
+                    if author not in board.keys():
+                        board[author] = {'GROS': 0, 'GARS': 0}
+                    
+                    board[author][response] += 1
+                    
+                    # Save file
+                    with open('board.json', 'w') as f:
+                        f.write(json.dumps(board, indent = 3))
+                    
+                    debug(f'Replied to ${author} on ${msg.guild.name}: {msg.content}')
+    
+    except Exception as e:
+        # debug(f'Raised exception: {type(e)}', 'err')
+        raise e
 
-    # Check for triggers
-    for response, trgs in triggers.items():
-        for trigger in trgs:
-            t = msg.content.lower()
-            
-            for char in string.punctuation:
-                t = t.replace(char, '')
-            
-            if t.strip().endswith(trigger):
-                await msg.reply(response)
-                
-                # Save to scoreboard
-                author = msg.author.name
-                
-                if author not in board.keys():
-                    board[author] = {'GROS': 0, 'GARS': 0}
-                
-                board[author][response] += 1
-                
-                # Save file
-                with open('board.json', 'w') as f:
-                    f.write(json.dumps(board, indent = 3))
+# Run server status as thread
+# def run_status(): import mc_status_bot
+# threading.Thread(target = run_status).start()
 
-client.run(open('token').read())    
+client.run(open('token').read(), log_handler = None)
+
+debug('Bot down')
+logs.close()
+
+# EOF
